4e362b16c9ce495ae5d722a5bd7fb3a8709350cf
Vu Thi Thanh Thanh <thanhvtt@toshiba-tsdv.com>
2020-09-07 17:04:49 +0700

    Load data form table to popup

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,38 @@
+$(document).on('click', '#searchBtn', buttonSearchOnClick);
 $(document).ready(function() {
 	$('.table').on('click', '#shownBtn', function(){
+		console.log("Show modal");
+		var $model = $('#exampleModal');
+		
+		var userName = $(this).parents("tr").find("#rowUserId").html();
+		$model.find("#inputId").val(userName);
+
+		var userName = $(this).parents("tr").find("#rowUserName").html();
+		$model.find("#inputName").val(userName);
+		
+		var userName = $(this).parents("tr").find("#rowUserAge").html();
+		$model.find("#inputAge").val(userName);
 		
 		$('#exampleModal').modal('show');
 	});
 	
-//	 $("#tableSearch").on('click', '#searchBtn', function()
-	 $("#tableSearch").on("keyup", function() {
-		    var value = $(this).val().toLowerCase();
-		    $("#myTable tr").filter(function() {
-		      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-		    });
-		  });
+//	 $("#tableSearch").on("keyup", function() {
+//		    var value = $(this).val().toLowerCase();
+//		    $("#myTable tr").filter(function() {
+//		      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//		    });
+//		  });
 	 
-	 $('#export').on('click', function(event) {
-		  event.preventDefault(); // To prevent following the link (optional)
+	 $('#export').on('click', function() {
 		  $('.table').tableExport({type:'csv'});
 		});
 	 
 });
+
+function buttonSearchOnClick() {
+	var value = $("#tableSearch").val().toLowerCase();
+	console.log(value);
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+}
